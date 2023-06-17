1f61b492b8d628897d2f4f9d886835b5a292eb0c
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2023-06-17 15:25:47 -0700

    csv instead of txt for history

--- autoJobApplierLinkedIn.py
+++ autoJobApplierLinkedIn.py
@@ -1,5 +1,6 @@
-import re
 import os
+import csv
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -66,52 +67,43 @@ def apply_to_jobs(keywords):
     if not logged_in:
         login(driver, wait)
     
-    # Track the job application history
-    history_file = ".allHistory.txt"
-    if not os.path.exists(history_file):
-        # Create .allHistory.txt file if it doesn't exist
-        open(history_file, 'w').close()
-
-    with open(history_file, 'r') as f:
-        application_history = f.read().splitlines()
-    
-    for keyword in keywords:
-        url = f"https://www.linkedin.com/jobs/search/?keywords={keyword}"
-        driver.get(url)
+    # Create or append to the CSV file
+    with open('all_quick_applied_history.csv', mode='a', newline='') as csvfile:
+        fieldnames = ['Job Title', 'Company', 'Application Link', 'Date and Time']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if csvfile.tell() == 0:
+            writer.writeheader()
         
-        # Wait until job listings are loaded
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "job-card-container")))
-        
-        # Find all job listings
-        job_listings = driver.find_elements(By.CLASS_NAME, "job-card-container")
-        
-        for job in job_listings:
-            # Extract job details
-            title = job.find_element(By.CLASS_NAME, "job-card-search__title").text
-            company = job.find_element(By.CLASS_NAME, "job-card-container__company-name").text
-            apply_button = job.find_element(By.CLASS_NAME, "job-card-container__apply-method")
-            application_link = apply_button.get_attribute("href")
+        for keyword in keywords:
+            url = f"https://www.linkedin.com/jobs/search/?keywords={keyword}"
+            driver.get(url)
             
-            # Check if job has already been applied
-            if application_link in application_history:
-                continue
+            # Wait until job listings are loaded
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "job-card-container")))
             
-            # Click on "Quick Apply" button
-            apply_button.click()
+            # Find all job listings
+            job_listings = driver.find_elements(By.CLASS_NAME, "job-card-container")
             
-            # Complete the application process (fill out forms, upload resume, etc.)
-            # You'll need to identify the necessary form fields and interact with them using Selenium
+            for job in job_listings:
+                # Extract job details
+                title = job.find_element(By.CLASS_NAME, "job-card-search__title").text
+                company = job.find_element(By.CLASS_NAME, "job-card-container__company-name").text
+                apply_button = job.find_element(By.CLASS_NAME, "job-card-container__apply-method")
+                application_link = apply_button.get_attribute("href")
+                
+                # Click on "Quick Apply" button
+                apply_button.click()
+                
+                # Complete the application process (fill out forms, upload resume, etc.)
+                # You'll need to identify the necessary form fields and interact with them using Selenium
+                
+                # Once the application is submitted successfully, add the application details to the CSV
+                writer.writerow({'Job Title': title, 'Company': company, 'Application Link': application_link, 'Date and Time': datetime.now()})
+                
+                # Go back to the job listings page
+                driver.back()
             
-            # Once the application is submitted successfully, add the application link to the history
-            application_history.append(application_link)
-            with open(history_file, 'a') as f:
-                f.write(application_link + '\n')
-            
-            # Go back to the job listings page
-            driver.back()
-        
     # Close the browser
     driver.quit()
-
 
 apply_to_jobs(keywords)

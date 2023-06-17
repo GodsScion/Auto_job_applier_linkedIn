7468fe0843d097b3885fee190dfb4342c5446e22
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2023-06-17 15:18:11 -0700

    made changes for login method

--- autoJobApplierLinkedIn.py
+++ autoJobApplierLinkedIn.py
@@ -1,15 +1,79 @@
+import re
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Example usage
+keywords = ["Junior Software Engineer", "Entry level Software Developer", "ReactJs Developer"]
+
+def is_logged_in(driver):
+    try:
+        sign_in_button = driver.find_element(By.LINK_TEXT, "Sign in")
+        sign_in_button.click()
+        return False
+    except:
+        return True
+
+def login(driver, wait):
+    while not is_logged_in(driver):
+        # Find the username and password fields and fill them with user credentials
+        username_field = wait.until(EC.presence_of_element_located((By.ID, "username")))
+        password_field = wait.until(EC.presence_of_element_located((By.ID, "password")))
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+
+        # Find the login submit button and click it
+        submit_button = driver.find_element(By.CSS_SELECTOR, ".btn__primary--large")
+        submit_button.click()
+
+        # Wait until the profile element is visible, indicating successful login
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "nav-item__profile-member-photo")))
+
+def find_default_profile_directory():
+    # List of default profile directory locations to search
+    default_locations = [
+        r"%LOCALAPPDATA%\Google\Chrome\User Data",
+        r"%USERPROFILE%\AppData\Local\Google\Chrome\User Data",
+        r"%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data"
+    ]
+
+    for location in default_locations:
+        profile_dir = os.path.expandvars(location)
+        if os.path.exists(profile_dir):
+            return profile_dir
+
+    return None
+
 def apply_to_jobs(keywords):
-    # Set up WebDriver (assuming you have the appropriate driver executable in your PATH)
-    driver = webdriver.Chrome()
+    # Set up WebDriver with Chrome Profile
+    options = Options()
+    profile_dir = find_default_profile_directory()
+    if profile_dir:
+        options.add_argument(f"--user-data-dir={profile_dir}")
+    else:
+        print("Default profile directory not found. Using a new profile.")
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()  # Maximize the browser window
     wait = WebDriverWait(driver, 10)
     
+    # Check if the user is logged in
+    logged_in = is_logged_in(driver)
+    
+    # If not logged in, perform the login process
+    if not logged_in:
+        login(driver, wait)
+    
     # Track the job application history
-    application_history = []
+    history_file = ".allHistory.txt"
+    if not os.path.exists(history_file):
+        # Create .allHistory.txt file if it doesn't exist
+        open(history_file, 'w').close()
+
+    with open(history_file, 'r') as f:
+        application_history = f.read().splitlines()
     
     for keyword in keywords:
         url = f"https://www.linkedin.com/jobs/search/?keywords={keyword}"
@@ -40,6 +104,8 @@ def apply_to_jobs(keywords):
             
             # Once the application is submitted successfully, add the application link to the history
             application_history.append(application_link)
+            with open(history_file, 'a') as f:
+                f.write(application_link + '\n')
             
             # Go back to the job listings page
             driver.back()
@@ -47,6 +113,5 @@ def apply_to_jobs(keywords):
     # Close the browser
     driver.quit()
 
-# Example usage
-keywords = ["Junior Software Engineer", "Entry level Software Developer", "ReactJs Developer"]
+
 apply_to_jobs(keywords)
--- setup.sh
+++ setup.sh
@@ -2,42 +2,65 @@
 
 # Check if Python is installed
 if ! command -v python &> /dev/null; then
-    echo "Python not found. Installing Python..."
-    # Download the latest Python installer for Windows (modify the URL based on your Python version)
-    curl -O "https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe"
-    # Run the Python installer silently
-    python-3.11.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-    # Clean up the installer
-    rm python-3.11.4-amd64.exe
+    # Install the latest stable Python version
+    echo "Python is not installed. Installing the latest stable version..."
+    PYTHON_URL="https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe"
+    PYTHON_FILE="python-installer.exe"
+    
+    wget -O $PYTHON_FILE $PYTHON_URL
+    cmd.exe /c start $PYTHON_FILE
+    
+    echo "Please follow the Python installation wizard."
+    echo "After the installation is complete, press Enter to continue."
+    read -p ""
+    
+    rm $PYTHON_FILE
 else
-    echo "Python already installed."
+    echo "Python is already installed."
 fi
 
-# Check if Chrome is installed
-if ! command -v "C:\Program Files\Google\Chrome\Application\chrome.exe" &> /dev/null; then
-    echo "Google Chrome not found. Installing Chrome..."
-    # Download the latest Chrome installer for Windows (modify the URL based on your Chrome version)
-    curl -O "https://dl.google.com/chrome/install/ChromeStandaloneSetup.exe"
-    # Run the Chrome installer silently
-    ChromeStandaloneSetup.exe --silent --install
-    # Clean up the installer
-    rm ChromeStandaloneSetup.exe
+# Check if Google Chrome is installed
+if ! command -v google-chrome &> /dev/null; then
+    # Download and install Google Chrome
+    echo "Google Chrome is not installed. Installing Google Chrome..."
+    CHROME_URL="https://dl.google.com/chrome/install/standalone/chrome_installer.exe"
+    CHROME_FILE="chrome-installer.exe"
+    
+    wget -O $CHROME_FILE $CHROME_URL
+    cmd.exe /c start $CHROME_FILE
+    
+    echo "Please follow the Google Chrome installation wizard."
+    echo "After the installation is complete, press Enter to continue."
+    read -p ""
+    
+    rm $CHROME_FILE
 else
-    echo "Google Chrome already installed."
+    echo "Google Chrome is already installed."
 fi
 
-# Install required packages
+# Install required Python packages
 pip install selenium
 
-# Download ChromeDriver (replace the URL with the appropriate version for your system)
-CHROME_DRIVER_URL="https://chromedriver.storage.googleapis.com/93.0.4577.15/chromedriver_win32.zip"
-curl -O "$CHROME_DRIVER_URL"
-unzip chromedriver_win32.zip
-# Move the chromedriver executable to a directory included in the PATH environment variable
-mv chromedriver.exe /usr/local/bin/
-# Clean up the downloaded archive
-rm chromedriver_win32.zip
+# Get the latest ChromeDriver version
+LATEST_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 
-# Provide instructions for the user
-echo "Setup completed successfully!"
-echo "You can now run your web scraping tool."
+# Download ChromeDriver
+CHROMEDRIVER_URL="https://chromedriver.storage.googleapis.com/${LATEST_VERSION}/chromedriver_win32.zip"
+CHROMEDRIVER_FILE="chromedriver.zip"
+CHROMEDRIVER_DIR="chromedriver"
+
+wget -O $CHROMEDRIVER_FILE $CHROMEDRIVER_URL
+unzip $CHROMEDRIVER_FILE -d $CHROMEDRIVER_DIR
+rm $CHROMEDRIVER_FILE
+
+# Get the absolute path to the current directory
+CURRENT_DIR=$(pwd)
+
+# Set up environment variables
+echo "export CHROME_DRIVER_PATH=${CURRENT_DIR}/${CHROMEDRIVER_DIR}/chromedriver.exe" >> ~/.bashrc
+echo "export PATH=\$PATH:${CURRENT_DIR}/${CHROMEDRIVER_DIR}" >> ~/.bashrc
+
+# Activate the environment variables in the current shell
+source ~/.bashrc
+
+echo "Setup complete. You can now use the web scraping tool."

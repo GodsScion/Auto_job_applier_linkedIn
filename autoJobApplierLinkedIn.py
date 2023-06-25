d1c7d0aa7c8a928386640640768e1584946e13c4
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2023-06-25 11:59:34 -0700

    lot's of changes

--- autoJobApplierLinkedIn.py
+++ autoJobApplierLinkedIn.py
@@ -1,5 +1,7 @@
 import os
 import csv
+from time import sleep
+from random import randint
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,29 +11,26 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # Example usage
 keywords = ["Junior Software Engineer", "Entry level Software Developer", "ReactJs Developer"]
+resumes = ["","","","",""]
 
-def is_logged_in(driver):
-    try:
-        sign_in_button = driver.find_element(By.LINK_TEXT, "Sign in")
-        sign_in_button.click()
-        return False
-    except:
-        return True
+username = "username@example.com" 
+password = "examplepassword"
 
-def login(driver, wait):
-    while not is_logged_in(driver):
-        # Find the username and password fields and fill them with user credentials
-        username_field = wait.until(EC.presence_of_element_located((By.ID, "username")))
-        password_field = wait.until(EC.presence_of_element_located((By.ID, "password")))
-        username_field.send_keys(username)
-        password_field.send_keys(password)
+# Set the amount of time to wait between each click
+click_gap = 0
 
-        # Find the login submit button and click it
-        submit_button = driver.find_element(By.CSS_SELECTOR, ".btn__primary--large")
-        submit_button.click()
 
-        # Wait until the profile element is visible, indicating successful login
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "nav-item__profile-member-photo")))
+
+
+def buffer(speed=0):
+    if speed<=0:
+        return
+    elif speed <= 1:
+        return sleep(randint(7,10)*0.1)
+    else:
+        return sleep(randint(9,18)*0.1)
+
+
 
 def find_default_profile_directory():
     # List of default profile directory locations to search
@@ -40,33 +39,66 @@ def find_default_profile_directory():
         r"%USERPROFILE%\AppData\Local\Google\Chrome\User Data",
         r"%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data"
     ]
-
     for location in default_locations:
         profile_dir = os.path.expandvars(location)
         if os.path.exists(profile_dir):
             return profile_dir
-
     return None
 
+
+
+def is_logged_in():
+    try:
+        sign_in_button = driver.find_element(By.LINK_TEXT, "Sign in")
+        return False
+    except Exception as e1:
+        try:
+            sign_in_button = driver.find_element(By.XPATH, '//button[@type="submit" and contains(text(), "Sign in")]')
+            return False
+        except Exception as e2:
+            print(e1,e2)
+            print("Didn't find Sign in link, so assuming user is logged in!")
+            return True
+
+
+
+def login():
+    while not is_logged_in():
+        # Find the username and password fields and fill them with user credentials
+        driver.get("https://www.linkedin.com/login")
+        # if username == "username@example.com":
+        #     print("Please complete login, job automation starts after 60 secs!")
+        #     sleep(60)
+        #     return True
+        try:
+            forgot_password_link = wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Forgot password?")))
+            try:
+                driver.find_element(By.ID, "username").send_keys(username)
+            except Exception as e:
+                print("Couldn't find username field.",e)
+            try:
+                driver.find_element(By.ID, "password").send_keys(password)
+            except Exception as e:
+                print("Couldn't find password field.",e)
+            # Find the login submit button and click it
+            driver.find_element(By.XPATH, '//button[@type="submit" and contains(text(), "Sign in")]').click()
+        except Exception as e1:
+            try:
+                profile_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "profile__details")))
+                profile_button.click()
+            except Exception as e2:
+                print(e1,"Couldn't Login!",e2)
+
+        try:
+            # Wait until the profile element is visible, indicating successful login
+            wait.until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Start a post")]')))
+        except Exception as e:
+            print("Seems like login attempt failed! Possibly due to wrong credentials or already logged in!",e)
+            break
+
+
+
 def apply_to_jobs(keywords):
-    # Set up WebDriver with Chrome Profile
-    options = Options()
-    profile_dir = find_default_profile_directory()
-    if profile_dir:
-        options.add_argument(f"--user-data-dir={profile_dir}")
-    else:
-        print("Default profile directory not found. Using a new profile.")
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()  # Maximize the browser window
-    wait = WebDriverWait(driver, 10)
-    
-    # Check if the user is logged in
-    logged_in = is_logged_in(driver)
-    
-    # If not logged in, perform the login process
-    if not logged_in:
-        login(driver, wait)
-    
     # Create or append to the CSV file
     with open('all_quick_applied_history.csv', mode='a', newline='') as csvfile:
         fieldnames = ['Job Title', 'Company', 'Application Link', 'Date and Time']
@@ -77,7 +109,16 @@ def apply_to_jobs(keywords):
         for keyword in keywords:
             url = f"https://www.linkedin.com/jobs/search/?keywords={keyword}"
             driver.get(url)
+
+            #Setting preferences
+            wait.until(EC.presence_of_element_located((By.XPATH, '//button[normalize-space()="All filters"]'))).click()
+            wait.until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Most recent")]'))).click()
             
+
+            # wait.until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Past 24 hours")]'))).click()
+
+            
+
             # Wait until job listings are loaded
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, "job-card-container")))
             
@@ -106,4 +147,33 @@ def apply_to_jobs(keywords):
     # Close the browser
     driver.quit()
 
-apply_to_jobs(keywords)
+
+
+
+# Set up WebDriver with Chrome Profile
+options = Options()
+profile_dir = find_default_profile_directory()
+if profile_dir:
+    options.add_argument(f"--user-data-dir={profile_dir}")
+else:
+    print("Default profile directory not found. Using a new profile.")
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()  # Maximize the browser window
+wait = WebDriverWait(driver, 10)
+
+
+
+def main():
+    try:
+        driver.get("https://www.linkedin.com/login")
+
+        # If not logged in, perform the login process
+        login()
+
+        apply_to_jobs(keywords)
+
+    except Exception as e:
+        print(e)
+        driver.quit()
+
+main()
---
+++ windows-setup.ps1
@@ -0,0 +1,50 @@
+# Check if Python is installed
+while (-not (python -V)) {
+    Write-Host "Python is not installed or not accessible!"
+    Write-Host "Please install Python and make sure it is added to your system's PATH environment variable."
+    Write-Host "Hold Ctrl and click on the link below or search 'Python Download' in your browser."
+    Write-Host "https://www.python.org/downloads/"
+    Write-Host "After installing Python and adding it to PATH, press Enter to continue."
+    Read-Host -Prompt ""
+}
+
+Write-Host "Python is installed."
+
+
+
+# Check if Google Chrome is installed
+while (-not (Test-Path -Path "C:\Program Files\Google\Chrome\Application\chrome.exe")) {
+    # Prompt user to install Google Chrome
+    Write-Host "Google Chrome is not installed or not installed in the default location."
+    Write-Host "Please make sure to install it before proceeding."
+    Write-Host "Please install Google Chrome to continue..."
+    Write-Host "Hold Ctrl and click on the link below or manually open a browser and search 'Google Chrome Download'."
+    Write-Host "https://www.google.com/chrome/"
+    Write-Host "After the installation is complete, press Enter to continue."
+    Read-Host -Prompt ""
+}
+
+Write-Host "Google Chrome is installed."
+
+# Install required Python packages
+pip install selenium
+
+# Get the latest ChromeDriver version
+$latestVersionUrl = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+$latestVersion = Invoke-WebRequest -Uri $latestVersionUrl -UseBasicParsing | Select-Object -ExpandProperty Content
+
+# Download ChromeDriver
+$chromeDriverUrl = "https://chromedriver.storage.googleapis.com/$latestVersion/chromedriver_win32.zip"
+$chromeDriverZip = "chromedriver.zip"
+$chromeDriverDir = "chromedriver"
+Invoke-WebRequest -Uri $chromeDriverUrl -OutFile $chromeDriverZip
+Expand-Archive -Path $chromeDriverZip -DestinationPath $chromeDriverDir
+Remove-Item -Path $chromeDriverZip
+
+# Set up environment variables
+$env:CHROME_DRIVER_PATH = "$($PWD.Path)\$chromeDriverDir\chromedriver.exe"
+$env:PATH += ";$($PWD.Path)\$chromeDriverDir"
+
+Write-Host "Setup complete. You can now use the web scraping tool."
+Read-Host -Prompt "Press any button to continue..."
+

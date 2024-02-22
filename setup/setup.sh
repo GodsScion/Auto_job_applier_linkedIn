a4e2d134c139f3535b903caebdb731b4e2a8e103
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2024-02-21 21:50:38 -0800

    License Link

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,6 +5,8 @@ LinkedIn:   https://www.linkedin.com/in/saivigneshgolla/
 Copyright (C) 2024 Sai Vignesh Golla
 
 License:    GNU Affero General Public License
+            https://www.gnu.org/licenses/agpl-3.0.en.html
+            
 GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
 
 '''

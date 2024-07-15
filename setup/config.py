6e254b8f3cadb9ed1f865fed6040b59b322f4d89
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-07-15 11:39:15 -0700

    Update config.py

--- setup/config.py
+++ setup/config.py
@@ -72,7 +72,7 @@ question_3 = []                    # (dynamic multiple select)
 '''
 
 sort_by = ""                       # "Most recent", "Most relevant" or ("" to not select) 
-date_posted = "Past Month"         # "Any time", "Past month", "Past week", "Past 24 hours" or ("" to not select)
+date_posted = "Past month"         # "Any time", "Past month", "Past week", "Past 24 hours" or ("" to not select)
 salary = ""                        # "$40,000+", "$60,000+", "$80,000+", "$100,000+", "$120,000+", "$140,000+", "$160,000+", "$180,000+", "$200,000+"
 
 easy_apply_only = True             # True or False

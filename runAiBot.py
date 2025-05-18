70fea1c06754b43339f3ce6e4e4191f811acc3f3
Kyxie <kyriexie@outlook.com>
2025-05-18 14:27:59 -0400

    fix: can't find pigination error

--- runAiBot.py
+++ runAiBot.py
@@ -252,8 +252,8 @@ def get_page_info() -> tuple[WebElement | None, int | None]:
     Function to get pagination element and current page number
     '''
     try:
+        ##> ------ Kyxie : kyriexie@outlook.com - Pagination problem fixed ------
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        sleep(2)
         pagination_element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
                 (By.CSS_SELECTOR, "ul.artdeco-pagination__pages.artdeco-pagination__pages--number")
@@ -263,6 +263,7 @@ def get_page_info() -> tuple[WebElement | None, int | None]:
         current_page = int(
             pagination_element.find_element(By.CSS_SELECTOR, "li.active.selected button span").text
         )
+        ##<
     except Exception as e:
         print_lg("Failed to find Pagination element, hence couldn't scroll till end!")
         pagination_element = None

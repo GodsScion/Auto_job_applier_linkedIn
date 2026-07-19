cfda82716797d2835f8eadcc561d546bf9e6e090
saidireddym <saidireddym@gmail.com>
2026-07-19 18:11:20 +0400

    test

--- runAiBot.py
+++ runAiBot.py
@@ -1,18 +1,18 @@
-'''
-Author:     Sai Vignesh Golla
-LinkedIn:   https://www.linkedin.com/in/saivigneshgolla/
+# '''
+# Author:     Sai Vignesh Golla
+# LinkedIn:   https://www.linkedin.com/in/saivigneshgolla/
 
-Copyright (C) 2024 Sai Vignesh Golla
+# Copyright (C) 2024 Sai Vignesh Golla
 
-License:    GNU Affero General Public License
-            https://www.gnu.org/licenses/agpl-3.0.en.html
+# License:    GNU Affero General Public License
+#             https://www.gnu.org/licenses/agpl-3.0.en.html
             
-GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
+# GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
 
-Support me: https://github.com/sponsors/GodsScion
+# Support me: https://github.com/sponsors/GodsScion
 
-version:    26.01.20.5.08
-'''
+# version:    26.01.20.5.08
+# '''
 
 
 # Imports
